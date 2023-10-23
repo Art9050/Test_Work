3ad4075a2e1dea018ab612cb2c1e4b9755efa5d1
Artem <118397890+Art9050@users.noreply.github.com>
2023-10-23 21:22:18 +0400

    Update main.sql

--- WhoIsBlogger (WIB)/main.sql
+++ WhoIsBlogger (WIB)/main.sql
@@ -3,23 +3,26 @@
 пользователи в возрастном диапазоне от 18 до 25 лет включительно
 пользователи в возрастном диапазоне от 26 до 35 лет включительно 
 */
-select 
-	avg(price) 
-From (
-  select 
-      purchaseId,
-      purchases.userId,
-      purchases.itemId,
-      dtd,
-      CAST(age as int),
-      price
-  from purchases
-  left JOIN users
-  	on purchases.userId = users.userId
-  left JOIN items
-  	on purchases.itemId = items.itemId
+WITH orders AS (
+	select 
+		purchaseId,
+		purchases.userId,
+		purchases.itemId,
+		dtd,
+		EXTRACT(YEAR FROM(AGE(age))) as age,
+		price
+	from purchases
+	left JOIN users
+	on purchases.userId = users.userId
+	left JOIN items
+	on purchases.itemId = items.itemId
 	)
+
+select avg(price)  from orders
 Where age BETWEEN 18 and 25
+union ALL
+select avg(price)  from orders
+Where age BETWEEN 26 and 35
 
 /*
 Б) в каком месяце года выручка от пользователей в возрастном 
